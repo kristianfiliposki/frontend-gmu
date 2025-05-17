713e2d3e40192d91b47f0a05d6fb2928e48153b9
kristianfiliposki <142878489+kristianfiliposki@users.noreply.github.com>
2025-05-17 18:45:42 +0200

    bozza-mock up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { BodyComponent } from './components/body/body.component';
+import { NgApexchartsModule } from 'ng-apexcharts';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SidebarComponent],
+  imports: [SidebarComponent,BodyComponent,NgApexchartsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
